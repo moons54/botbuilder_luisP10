1408589c4af9625132e14276f202f9c8dbd9ff1d
aurelienmimouni <aurelien.mimouni@gmail.com>
2021-11-19 13:21:13 +0100

    'modif config.py'

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "a2f9c630-6d4c-46a8-be15-77ceb3be3171")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "eb3741a4b3004f0fbbc5b9552465b4b6")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "aba766fa-7667-434e-b4e4-b94803690524"
+        "AppInsightsInstrumentationKey", ""
     )
